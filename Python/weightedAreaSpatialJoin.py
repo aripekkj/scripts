b9040a42467ba5085845d86b7eb00cdd9af98df3
aripekkj <31698615+aripekkj@users.noreply.github.com>
2020-09-08 16:06:41 +0300

    Add files via upload

--- Python/weightedAreaSpatialJoin.py
+++ Python/weightedAreaSpatialJoin.py
@@ -3,9 +3,9 @@
 Created on Mon Sep  7 12:11:55 2020
 
 
-Weighted mean in spatial join
+Compute area weighted mean for areas in spatial join
 
-Note: not ready
+
 
 @author: aripekkj
 """
@@ -15,13 +15,15 @@ import geopandas as gpd
 
 
 # filepath
-fp = r'C:\Users\HY\Documents\ProtectedAreas\INSTAT\mdg_adm1_region_df.shp'
-fp2 = r'C:\Users\HY\Documents\ProtectedAreas\WDPA\WDPA_June2020_Mada_final_PAs2.shp'
-out_fp = r'C:\Users\HY\Documents\ProtectedAreas\INSTAT\mdg_adm1_region_df_test_overlay.shp' 
+fp = r'E:\LocalData\aripekkj\ProtectedAreas\Madagascar\INSTAT\mdg_adm1_region_df.shp'
+fp2 = r'E:\LocalData\aripekkj\ProtectedAreas\Madagascar\WDPA\WDPA_June2020_Mada_final_PAs2.shp'
+fp3 = r'E:\LocalData\aripekkj\ProtectedAreas\Madagascar\sjoin\WDPA_fire_w_region_df_pop_acc_means_finalPAs.shp'
+out_fp = r'E:\LocalData\aripekkj\ProtectedAreas\Madagascar\INSTAT\mdg_adm1_region_df_w_spatial_join.shp' 
 
 # read file
 region = gpd.read_file(fp)
 pa = gpd.read_file(fp2)
+join_pa = gpd.read_file(fp3)
 
 # overlay region by wdpa. result has attributes from both layers and areas where single PA polygon intersects over many regions are individual polygons
 region_ol = gpd.overlay(region, pa) 
@@ -40,7 +42,7 @@ pa_utm = reProject(pa, 32738)
 region_utm['area_km2'] = region_utm['geometry'].area / 10**6 
 pa_utm['area_km2'] = pa_utm['geometry'].area / 10**6
 
-# empty column for area proportion
+# empty column for area proportion. This will be used as weight
 region_utm['area_prop'] = ""
 
 # loop through wdpa_pid in region_utm DataFrame and select corresponding PA from WDPA layer. Then compute the percentage how much the region covers of the whole PA
@@ -58,6 +60,52 @@ region_utm['Nr_of_HH_w'] = region_utm['Nr_of_HH'] * region_utm['area_prop']
 region_utm['PoorFood_w'] = region_utm['Poor_food_'] * region_utm['area_prop']
 region_utm['noFoodAl_w'] = region_utm['no_food_al'] * region_utm['area_prop']
 region_utm['FoodSecA_w'] = region_utm['food_sec_a'] * region_utm['area_prop']
+
+
+# function to count sum and group them by WDPA_PID
+def groupBy(df, column_name):
+    
+    # group by WDPA_PID
+    df_group = df.groupby('WDPA_PID')[column_name].sum()
+    
+    # convert Series to DataFrame
+    df_out = pd.Series.to_frame(df_group)
+    # reset index
+    df_out = df_out.reset_index()
+    
+    return df_out;
+
+# group by
+econ_det_w_sum = groupBy(region_utm, 'Econ_det_w')
+nrHH_w = groupBy(region_utm, 'Nr_of_HH_w')
+poorfood_w = groupBy(region_utm, 'PoorFood_w')
+noFood_w = groupBy(region_utm, 'noFoodAl_w')
+FoodSecA_W = groupBy(region_utm, 'FoodSecA_w')
+
+# join list
+join_list = [econ_det_w_sum, nrHH_w, poorfood_w, noFood_w, FoodSecA_W]
+
+# join grouped dataframes by wdpa id 
+for i in join_list:
+    
+    join_pa = pd.merge(join_pa, i, how='outer', on='WDPA_PID')
+
+# save to shapefile
+join_pa.to_file(out_fp, driver='ESRI Shapefile')
+
+
+
+# to find certain protected area for double checking results
+test = region_utm[region_utm['WDPA_PID'] == '555548845']
+print(test[['Econ_deter', 'area_km2', 'area_prop', 'Econ_det_w']])
+print(sum(test['Econ_deter']) / len(test['Econ_deter']))
+print(sum(test['Econ_det_w']))
+
+
+
+
+
+
 
 
 
